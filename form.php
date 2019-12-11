491ac5f72ccf1ac9ab104444d15dd8e6d814cf81
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2019-12-11 10:58:08 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<label for="email">Enter your email: </label>
+	<input type="text" id="name" name="name" required minlength="4" maxlength="10" size="10">
+	<input type="submit">
+</body>
+</html>
